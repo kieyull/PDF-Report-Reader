c1f95f1a6a4c7736066a41e546087413e7eadc7a
kieyull <gunbyk@gmail.com>
2018-12-13 17:13:17 -0600

    Fixed cleaning and created pandas dataframe

--- reader.py
+++ reader.py
@@ -2,13 +2,12 @@ import PyPDF2
 import re
 import pandas as pd
 
-pdf_obj = open('sept18.pdf', 'rb')
-pdf_reader = PyPDF2.PdfFileReader(pdf_obj)
-pages = pdf_reader.numPages
+pdf = open('sept18.pdf', 'rb')
+reader = PyPDF2.PdfFileReader(pdf)
+pages = reader.numPages
 
-first_page = pdf_reader.getPage(0)
+first_page = reader.getPage(0)
 first_page_text = first_page.extractText()
-
 info = re.findall(r'\b([\w/.,#\' ]*)\n', first_page_text)
 
 # Remove the beginning data
@@ -21,9 +20,57 @@ x_axis = info[:7]
 del info[:7]
 del info[-3:]
 
-# Build lists for all data
-# groups = info[::5]
-# bins = info[1::5]
-# utilization = info[2::5]
-# rate = info[3::5]
-# payout = info[4::5]
+def check_vals(tmp, current_val):
+    # Pattern [Group Number, BIN, Program, Date, Utilization, Rate, Ammount]
+    if not re.match(r'^[A-Z\d]*$', tmp[0]):
+        print("Error: Incorrect Group Number - {}".format(tmp[0]))
+        del info[current_val - 6]
+        return False
+        
+    if not re.match(r'^[0-9]*$', tmp[1]):
+        print("Error: Incorrect Bin Number - {}".format(tmp[1]))
+        del info[current_val - 5]
+        return False
+        
+    if not re.match(r'^[A-Za-z-.\' ]*$', tmp[2]):
+        print("Error: Incorrect Program Name - {}".format(tmp[2]))
+        del info[current_val - 4]
+        return False
+    
+    if not re.match(r'([0-9]\/[0-9])', tmp[3]):
+        print("Error: Incorrect Date - {}".format(tmp[3]))
+        del info[current_val - 3]
+        return False
+        
+    if not re.match(r'^[0-9]*$', tmp[4]):
+        print("Error: Incorrect Utilization Number - {}".format(tmp[4]))
+        del info[current_val - 2]
+        return False
+    
+    if not re.match(r'^(\d{2}(,\d{3})*|(\d+))(\.\d{2})$', tmp[5]):
+        print("Error: Incorrect Rate Number - {}".format(tmp[5]))
+        del info[current_val - 1]
+        return False
+        
+    if not re.match(r'^(\d{2}(,\d{3})*|(\d+))(\.\d{2})$', tmp[6]):
+        print("Error: Incorrect Payout Number - {}".format(tmp[6]))
+        del info[current_val]
+        return False
+    return True
+
+df = []
+for i in range(len(info)):
+    if i != 0 and i % 7 == 0:
+        if i == 7:
+            # Check the values of the data
+            tmp = info[:7]
+            if check_vals(tmp, i):
+                df.append(info[:7])
+            else:
+                break
+        else:
+            tmp = info[i-7:i]
+            if check_vals(tmp, i):
+                df.append(info[i-7:i])
+
+comms_df = pd.DataFrame(df, columns=x_axis)
